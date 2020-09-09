65ea10758f2b0cedbebd50147959744901c32ad3
Thomas van Dijk <tcdijk@gmail.com>
2020-09-09 16:20:03 +0200

    Change package name to miniipe

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="miniipe-tvd",
+    name="miniipe",
     version="0.1.4",
     author="Thomas van Dijk",
     author_email="tvdmaps@gmail.com",
